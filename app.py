2874b319fa0970896e792e47379006547f3c337b
sanpoooyoshi <growthisoftenapainfulprocesssh@gmail.com>
2025-08-25 12:58:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ with col2:
 # STEP5
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("images/step5.png", width=120)
+    st.image("images/step4.png", width=120)
 with col2:
     st.markdown("### 📄 STEP5: 出品したい国の mass_upload_***_basic_template.xlsx をアップロード")
     template_path = st.file_uploader(label="", type=["xlsx"], key="template")
