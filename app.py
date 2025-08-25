7dcf384b19250904b9eb0a573aa663cf50e72a5b
sanpoooyoshi <growthisoftenapainfulprocesssh@gmail.com>
2025-08-25 19:46:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if basic_info_path and sales_info_path and media_info_path and shipment_info_pat
 
     # 値を埋め込む
     template_df_norm.loc[start_row:start_row + num_ids - 1, "et_title_variation_integration_no"] = product_ids.values
-    template_df_norm.loc[start_row:start_row + num_ids - 1, "et_title_variation_id"] = variation_ids.values
+    #template_df_norm.loc[start_row:start_row + num_ids - 1, "et_title_variation_id"] = variation_ids.values
     template_df_norm.loc[start_row:start_row + num_ids - 1, "ps_product_name"] = product_names.values
     template_df_norm.loc[start_row:start_row + num_ids - 1, "ps_sku_short"] = skus.values
     template_df_norm.loc[start_row:start_row + num_ids - 1, "ps_price"] = variation_prices.values
